2ce6e02888e4213d17ca3173da7e13c73e7dc124
Aniuyyds <90816904+Aniuyyds@users.noreply.github.com>
2023-04-08 14:56:42 +0800

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -170,4 +170,16 @@ $(document).ready(function() {
 
   // 绑定Enter键盘事件
   chatInput.on("keydown",handleEnter);
+  
+  // 禁用右键菜单
+  document.addEventListener('contextmenu',function(e){
+    e.preventDefault();  // 阻止默认事件
+  });
+
+  // 禁止键盘F12键
+  document.addEventListener('keydown',function(e){
+    if(e.key == 'F12'){
+        e.preventDefault(); // 如果按下键F12,阻止事件
+    }
+  });
 });
