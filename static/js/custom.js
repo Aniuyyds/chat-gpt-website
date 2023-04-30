c8c8a5ab8552c79a6e298649409b728836fb9de1
Aniuyyds <90816904+Aniuyyds@users.noreply.github.com>
2023-05-01 02:27:13 +0800

    Add files via upload

--- static/js/custom.js
+++ static/js/custom.js
@@ -46,48 +46,120 @@ $(document).ready(function() {
   var chatWindow = $('#chatWindow');
 
   // 存储对话信息,实现连续对话
-  var messages = []
+  var messages = [];
 
-  // 转义html代码，防止在浏览器渲染
+  // 检测是否是html代码的标志变量
+  var checkHtmlFlag = false;
+
+  // 检查返回的信息是否是正确信息
+  var resFlag = true
+
+  // 转义html代码(对应字符转移为html实体)，防止在浏览器渲染
   function escapeHtml(html) {
-    var text = document.createTextNode(html);
-    var div = document.createElement('div');
+    let text = document.createTextNode(html);
+    let div = document.createElement('div');
     div.appendChild(text);
     return div.innerHTML;
   }
 
-  // 添加消息到窗口
-  function addMessage(message,imgName) {
+  // 判断输出内容是否包含html标签
+  function checkHtmlTag(str) {
+    let pattern = /<\s*\/?\s*[a-z]+(?:\s+[a-z]+=(?:"[^"]*"|'[^']*'))*\s*\/?\s*>/i;  // 匹配HTML标签的正则表达式
+    return pattern.test(str); // 返回匹配结果
+  }
+  
+  // 添加请求消息到窗口
+  function addRequestMessage(message) {
     $(".answer .tips").css({"display":"none"});    // 打赏卡隐藏
     chatInput.val('');
-    var escapedMessage;
-    if (imgName == "avatar.png"){
-      escapedMessage= escapeHtml(message);  // 对请求message进行转义，防止输入的是html被浏览器渲染
-    }else if(imgName == "chatgpt.png"){
-      escapedMessage= marked(message);  // 使用marked.js对响应message的markdown格式转换为html
-    }
-    var messageElement = $('<div class="row message-bubble"><img class="chat-icon" src="./static/images/' + imgName + '"><div class="message-text">' +  escapedMessage + '</div></div>');
-    chatWindow.append(messageElement);
-    chatWindow.animate({ scrollTop: chatWindow.prop('scrollHeight') }, 500);
-  }
-
-
-  // 请求失败不用转义html
-  function addFailMessage(message) {
-    $(".answer .tips").css({"display":"none"});      // 打赏卡隐藏
-    chatInput.val('');
-    var messageElement = $('<div class="row message-bubble"><img class="chat-icon" src="./static/images/chatgpt.png"><p class="message-text">' +  message + '</p></div>');
-    chatWindow.append(messageElement);
+    let escapedMessage = escapeHtml(message);  // 对请求message进行转义，防止输入的是html而被浏览器渲染
+    let requestMessageElement = $('<div class="row message-bubble"><img class="chat-icon" src="./static/images/avatar.png"><div class="message-text request">' +  escapedMessage + '</div></div>');
+    chatWindow.append(requestMessageElement);
+    let responseMessageElement = $('<div class="row message-bubble"><img class="chat-icon" src="./static/images/chatgpt.png"><div class="message-text response"><span class="loading-icon"></span></div></div>');
+    chatWindow.append(responseMessageElement);
     chatWindow.animate({ scrollTop: chatWindow.prop('scrollHeight') }, 500);
   }
   
+  // 添加响应消息到窗口,流式响应此方法会执行多次
+  function addResponseMessage(message) {
+    let lastResponseElement = $(".message-bubble .response").last();
+    lastResponseElement.empty();
+    let escapedMessage;
+    if(checkHtmlTag(message)){  // 如果是html代码
+      escapedMessage = marked(escapeHtml(message)); 
+      checkHtmlFlag = true;
+    }else{
+      escapedMessage = marked(message);  // 响应消息markdown实时转换为html
+      checkHtmlFlag = false;
+    }
+    lastResponseElement.append(escapedMessage);
+    chatWindow.scrollTop(chatWindow.prop('scrollHeight'));
+  }
+
+  // 添加失败信息到窗口
+  function addFailMessage(message) {
+    let lastResponseElement = $(".message-bubble .response").last();
+    lastResponseElement.empty();
+    lastResponseElement.append(message);
+    chatWindow.scrollTop(chatWindow.prop('scrollHeight'));
+    messages.pop() // 失败就让用户输入信息从数组删除
+  }
+  
+
+  // 发送请求获得响应
+  async function sendRequest(data) {
+    const response = await fetch('https://open.aiproxy.xyz/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + data.apiKey
+      },
+      body: JSON.stringify({
+        "messages": data.prompt,
+        "model": "gpt-3.5-turbo",
+        "max_tokens": 2048,
+        "temperature": 0.5,
+        "top_p": 1,
+        "n": 1,
+        "stream": true
+      })
+    }); 
+  
+    const reader = response.body.getReader();
+    let res;
+    let str = '';
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      res = new TextDecoder().decode(value).replace(/data: /g, '').replace("[DONE]",'');
+      const lines = res.trim().split(/[\n]+(?=\{)/);
+      for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        const jsonObj = JSON.parse(line);
+        if (jsonObj.choices && jsonObj.choices[0].delta.content) {
+          str += jsonObj.choices[0].delta.content;
+          addResponseMessage(str);
+          resFlag = true;
+        }else{
+          if(jsonObj.error){
+            addFailMessage('<p class="error">' + jsonObj.error.type + " : " + jsonObj.error.message + '</p>');
+            resFlag = false;
+          }
+        } 
+      }
+    }
+    return str;
+  }
+
   // 处理用户输入
   chatBtn.click(function() {
     // 解绑键盘事件
     chatInput.off("keydown",handleEnter);
     
     // 保存api key与对话数据
-    var data = {
+    let data = {
       "apiKey" : "", // 这里填写固定 apiKey
     }
    
@@ -107,7 +179,7 @@ $(document).ready(function() {
 
     }
 
-    var message = chatInput.val();
+    let message = chatInput.val();
     if (message.length == 0){
       common_ops.alert("请输入内容！",function(){
         chatInput.val('');
@@ -115,61 +187,39 @@ $(document).ready(function() {
         chatInput.on("keydown",handleEnter);
       })
       return
-    }else{
-      message = message
     }
 
-
-    addMessage(message,"avatar.png");
-
+    addRequestMessage(message);
     // 将用户消息保存到数组
     messages.push({"role": "user", "content": message})
-
     // 收到回复前让按钮不可点击
     chatBtn.attr('disabled',true)
 
-    data.prompt = messages
-
-    // 发送信息到后台
-    $.ajax({
-      url: 'https://open.aiproxy.xyz/v1/chat/completions',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + data.apiKey
-      },
-      data: JSON.stringify({
-        "messages": data.prompt,
-        "model": "gpt-3.5-turbo",
-        "max_tokens": 2048,
-        "temperature": 0.5,
-        "top_p": 1,
-        "n": 1
-      }),
-      success: function(res) {
-        const resp = res["choices"][0]["message"];
-       
-        addMessage(resp.content,"chatgpt.png");
-        // 收到回复，让按钮可点击
-        chatBtn.attr('disabled',false)
-        // 重新绑定键盘事件
-        chatInput.on("keydown",handleEnter);
-        // 将回复添加到数组
-        messages.push(resp)
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        addFailMessage('<span style="color:red;">' + '出错啦！请稍后再试!' + '</span>');
-        chatBtn.attr('disabled',false)
-        chatInput.on("keydown",handleEnter);
-        messages.pop() // 失败就让用户输入信息从数组删除
+    data.prompt = messages;
+    
+    sendRequest(data).then((res) => {
+      if(resFlag){
+        messages.push({"role": "assistant", "content": res});
+      }
+      // 收到回复，让按钮可点击
+      chatBtn.attr('disabled',false)
+      // 重新绑定键盘事件
+      chatInput.on("keydown",handleEnter); 
+      if (checkHtmlFlag) {
+        let lastResponseElement = $(".message-bubble .response").last();
+        let lastResponseHtml = lastResponseElement.html();
+        let newLastResponseHtml = lastResponseHtml.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g, "&").replace(/&#39;/g, "'").replace(/&quot;/g, "\"");
+        lastResponseElement.html(newLastResponseHtml);
       }
     });
-  });
+
+  });  
 
   // Enter键盘事件
   function handleEnter(e){
     if (e.keyCode==13){
       chatBtn.click();
+      e.preventDefault();  //避免回车换行
     }
   }
 
