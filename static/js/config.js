539f631c7909cdb239fe0fffa173cb15bb368eab
Aniuyyds <90816904+Aniuyyds@users.noreply.github.com>
2023-05-14 23:10:14 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 const config = {
     // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
     url: "https://open.aiproxy.xyz/v1/chat/completions",  // gpt3.5代理接口
-    apiKey: "c2stWG1nU0FtTTRkRTluVUdlc1NLcTRUM0JsYmtGSnFCallydUJlNHRScXl0U1hNTzQ0"  // openAi Base64 编码 apiKey
+    apiKey: ""  // openAi Base64 编码 apiKey
 };  
